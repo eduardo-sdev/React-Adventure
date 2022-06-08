e6b9447c49b8571a037a1ad2ce454f9dd4bc3774
eduardo-developer01 <eduardosilvaprogramador@gmail.com>
2022-06-08 06:17:54 -0300

    aula 11 comp in react

---
+++ react-components/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { CompUm } from './comp/CompUm'
+
+function App() {
+  return (
+      <>
+        <CompUm />
+        <CompUm />
+      </>
+  )
+}
+
+export default App
---
+++ react-components/src/comp/CompUm.jsx
@@ -0,0 +1,8 @@
+export const CompUm = () => {
+
+    return (
+        <div class="comp">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, mollitia.
+        </div>
+    )
+}
